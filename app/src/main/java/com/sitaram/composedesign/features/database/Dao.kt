c63ab400dcdf864198fcdafb45a22617cd55b3ef
Sita Ram Thing <sitaramthing667@gmail.com>
2023-06-27 10:37:56 +0545

    Create the user DAO

--- app/src/main/java/com/sitaram/composedesign/features/database/Dao.kt
+++ app/src/main/java/com/sitaram/composedesign/features/database/Dao.kt
@@ -1,4 +1,18 @@
 package com.sitaram.composedesign.features.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import io.reactivex.rxjava3.core.Completable
+
+@SuppressWarnings("AndroidUnresolvedRoomSqlReference")
+@Dao
 interface Dao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun registerUser(userList: List<UserPojo>?): Completable?
+
+    @Query("SELECT EXISTS(SELECT * FROM user WHERE user_name =:userName AND user_password =:userPassword)")
+    fun getLoginUser(userName: String, userPassword: String): Boolean?
 }
--- app/src/main/java/com/sitaram/composedesign/features/database/UserPojo.kt
+++ app/src/main/java/com/sitaram/composedesign/features/database/UserPojo.kt
@@ -12,19 +12,19 @@ class UserPojo {
     @ColumnInfo(name = "userId")
     val id: Int? = null
 
-    @ColumnInfo(name = "userEmail")
-    var email: String? = null
+    @ColumnInfo(name = "user_email")
+    var userEmail: String? = null
 
-    @ColumnInfo(name = "userName")
-    var name: String? = null
+    @ColumnInfo(name = "user_name")
+    var userName: String? = null
 
-    @ColumnInfo(name = "userPassword")
-    var password: String? = null
+    @ColumnInfo(name = "user_password")
+    var userPassword: String? = null
 
     // constructor
     constructor(email: String, name: String, password: String){
-        this.email = email
-        this.name = name
-        this.password = password
+        this.userEmail = email
+        this.userName = name
+        this.userPassword = password
     }
 }
